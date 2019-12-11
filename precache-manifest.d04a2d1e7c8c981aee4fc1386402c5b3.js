a45b3439e5f05342c14bc5eee1e978adf21cb610
Dashiell Bell <dbell@btr.pm>
2019-12-11 17:35:17 +0100

    Updates

---
+++ precache-manifest.d04a2d1e7c8c981aee4fc1386402c5b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cb2cb19d1ed83be3aea2c3a17d905a",
+    "url": "/supersimple-react-globalstore/index.html"
+  },
+  {
+    "revision": "901622e453eee139142c",
+    "url": "/supersimple-react-globalstore/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8842ac93aaf52ca9de02",
+    "url": "/supersimple-react-globalstore/static/js/2.09aa8b99.chunk.js"
+  },
+  {
+    "revision": "122ed62217594dce163925ee1e06b024",
+    "url": "/supersimple-react-globalstore/static/js/2.09aa8b99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "901622e453eee139142c",
+    "url": "/supersimple-react-globalstore/static/js/main.96c7f0a8.chunk.js"
+  },
+  {
+    "revision": "b78e6203863db496b706",
+    "url": "/supersimple-react-globalstore/static/js/runtime-main.453905f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/supersimple-react-globalstore/static/media/logo.5d5d9eef.svg"
+  }
+]);
